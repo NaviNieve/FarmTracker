5acd71cb03653d1c8ead3a5dc77c786e12ff9ebe
Josh <56569329+NaviNieve@users.noreply.github.com>
2025-09-22 15:21:21 -0700

    Update #1 Basic Setup of Plugin

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Example
-An example greeter plugin
+# Farm Tracker
+Keeps track of amounts, prices, diseased, dead, bought, sold and displays amount made, as well as an optional graph.
--- src/main/java/com/farmtracker/FarmTrackerConfig.java
+++ src/main/java/com/farmtracker/FarmTrackerConfig.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.farmtracker;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup("example")
-public interface ExampleConfig extends Config
+public interface FarmTrackerConfig extends Config
 {
 	@ConfigItem(
 		keyName = "greeting",
